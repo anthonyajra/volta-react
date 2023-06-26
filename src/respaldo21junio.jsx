76c6249dee706f03a7e53724257274775b8d7dfe
anthonyajra <ajra2427@ueavolta.edu.ec>
2023-06-26 09:37:08 -0500

    first commit

---
+++ src/respaldo21junio.jsx
@@ -0,0 +1,10 @@
+const random = () => {
+    const vec = new Array(5);
+    for (let i = 0; i < array.lengt; i++) {
+      vec[i] = Math.trunc(Math.random() * 10)  
+      setNumero(vec)
+      
+    }
+  }
+  
+  const [numeros, setNumeros] = useState([0,1,0,0,0])
